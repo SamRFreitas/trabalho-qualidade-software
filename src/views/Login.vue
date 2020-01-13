c401da28e3a1cbf3fae72c9b9118c244a9bc2335
SamRFreitas <samuelrfreitassi>
2020-01-13 07:21:15 -0300

    OK

---
+++ src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+
+        <input type="text" placeholder="Email">
+        <br>
+        <input type="text" placeholder="Senha">
+        <br>
+
+        <div class="btn-login">
+            <button>Login</button>
+        </div>
+
+        <p> 
+            Você não tem conta?
+            <span>
+                <router-link to="registrar"> crie uma conta</router-link>
+            </span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "login",
+    data(){
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style>
+input {
+    height: 40px;
+    width: 250px;
+    margin-bottom: 10px;
+    padding-left: 10px;
+}
+
+button{
+    height: 40px;
+    width: 100px;
+    border: none;
+    border-color: none;
+    background-color: #55D6BE;
+    cursor: pointer;  
+    color: #FFF;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login">
+        <h1>Crie uma nova Conta</h1>
+
+        <input type="text" placeholder="Email">
+        <br>
+        <input type="text" placeholder="Senha">
+        <br>
+
+        <div class="btn-registrar">
+            <button>Registrar</button>
+        </div>
+
+        <p> 
+            ou retorne ao
+            <span>
+                <router-link to="login">login</router-link> 
+            </span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "signUp",
+    data(){
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style>
+.btn-registrar button{
+    background-color: #2E5EAA;
+    color: #FFF;
+}
+</style>
